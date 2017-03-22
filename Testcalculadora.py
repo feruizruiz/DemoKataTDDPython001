5d1180dd7f6a44713d51178bb757b2e442c09c5a
flxruizruiz <fe.ruiz@uniandes.edu.co>
2017-03-22 14:06:47 -0500

    paso 2 : se crea  una cadena con un numero diferente (ROJO)

--- Testcalculadora.py
+++ Testcalculadora.py
@@ -7,3 +7,7 @@ class TestCalculadora(TestCase):
 
     def test_sumar_unacadena(self):
         self.assertEqual(Calculadora().sumar("1"),1,"un numero")
+
+    def test_sumar_unacadenaUnNumero(self):
+        self.assertEqual(Calculadora().sumar("1"),1,"un numero")
+        self.assertEqual(Calculadora().sumar("2"), 2, "un numero")
