8266814591bd8c5801295b27561c2f6ea4b62270
flxruizruiz <fe.ruiz@uniandes.edu.co>
2017-03-22 05:35:08 -0500

    Paso 2 : Se crea Test cadena una cadena (VERDE)

--- Testcalculadora.py
+++ Testcalculadora.py
@@ -4,3 +4,6 @@ from Calculadora import Calculadora
 class TestCalculadora(TestCase):
     def test_sumar(self):
         self.assertEqual(Calculadora().sumar(""),0,"cadena vacia")
+
+    def test_sumar_unacadena(self):
+        self.assertEqual(Calculadora().sumar("1"),1,"un numero")
